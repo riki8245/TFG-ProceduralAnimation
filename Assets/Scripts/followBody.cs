a3edf67cb05a4530d5ff14cb71d53856b3aacfea
riki8245 <56740262+riki8245@users.noreply.github.com>
2021-06-01 12:38:20 +0200

    Rigging Fixed and Ready for switching part

---
+++ Assets/Scripts/followBody.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+
+public class followBody : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
